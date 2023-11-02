8988f66055c7e215eaa1397b8823392aeb697b41
Somyani Ghimire <ghimirsi@mail.uc.edu>
2023-11-03 02:47:27 +0545

    All changes done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult GetName()
+        {
+            var myName = new { Name = "Somyani Ghimire" }; 
+            return Ok(myName);
+        }
+    }
+}
